ff9e0484f36cc47307d3236abf0958326fa4d72c
Justin Fay <justin.fay@vervint.com>
2025-06-09 23:07:10 -0400

    Enhance SensorPollingWorker with dependency injection for aggregator, sensor client, and reading repository; improve logging and reading processing

--- Atmos/Application/Workers/SensorPollingWorker.cs
+++ Atmos/Application/Workers/SensorPollingWorker.cs
@@ -1,23 +1,45 @@
+using Application.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Interfaces;
+
 namespace Application;
 
 public class SensorPollingWorker : BackgroundService
 {
     private readonly ILogger<SensorPollingWorker> _logger;
+    private readonly ISensorClient _sensorClient;
+    private readonly IAggregator _aggregator;
+    private readonly IReadingRepository _readingRepository;
+    private readonly IMapper _mapper;
 
-    public SensorPollingWorker(ILogger<SensorPollingWorker> logger)
+    public SensorPollingWorker(ILogger<SensorPollingWorker> logger, IAggregator aggregator, ISensorClient sensorClient, IMapper mapper, IReadingRepository readingRepository)
     {
         _logger = logger;
+        _aggregator = aggregator;
+        _sensorClient = sensorClient;
+        _mapper = mapper;
+        _readingRepository = readingRepository;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _logger.LogDebug("SensorPollingWorker is starting.");
+        
+        // TODO: Add error handling
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (_logger.IsEnabled(LogLevel.Information))
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            }
-
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+           
+            var latestReadingDto = await _sensorClient.GetReadingAsync(stoppingToken);
+            _logger.LogDebug("Latest reading received: {reading}", latestReadingDto);
+            
+            var readingEntity = _mapper.Map<Reading>(latestReadingDto);
+            
+            var saveTask = _readingRepository.CreateAsync(readingEntity, stoppingToken);
+            var processTask = _aggregator.ProcessReadingAsync(readingEntity);
+            await Task.WhenAll(saveTask, processTask);    
+            
             await Task.Delay(1000, stoppingToken);
         }
     }
