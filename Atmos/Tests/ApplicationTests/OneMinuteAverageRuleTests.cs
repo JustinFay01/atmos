3c3868f0153827438b64c99bb1567d61846e2f15
Justin Fay <justin.fay@vervint.com>
2025-06-12 15:54:51 -0400

    On Average Calculation testing

--- Atmos/Tests/ApplicationTests/OneMinuteAverageRuleTests.cs
+++ Atmos/Tests/ApplicationTests/OneMinuteAverageRuleTests.cs
@@ -5,13 +5,13 @@ using Application.Rules;
 
 namespace Tests.ApplicationTests;
 
-public class MetricUpdateRuleTests : BaseTest<OneMinuteAverageRule>
+public class OneMinuteAverageRuleTests : BaseTest<OneMinuteAverageRule>
 {
     private readonly DateTime _now = new(2023, 1, 1, 1, 0, 0, DateTimeKind.Utc);
 
     #region Early Exit Conditions
 
-    public MetricUpdateRuleTests()
+    public OneMinuteAverageRuleTests()
     {
         Subject = new OneMinuteAverageRule(LoggerMock.Object);
     }
@@ -104,5 +104,55 @@ public class MetricUpdateRuleTests : BaseTest<OneMinuteAverageRule>
 
     #endregion
 
+    #region Average Calculation
+
+    [Test]
+    public async Task OneMinuteAverageRule_Integer_CalculatesCorrectOneMinuteAverage()
+    {
+        // Arrange
+        var aggregate = new MetricAggregate
+        {
+            RecentReadings = new ConcurrentQueue<Metric>([
+                new Metric { Timestamp = _now.AddSeconds(-50), Value = 10 },
+                new Metric { Timestamp = _now.AddSeconds(-40), Value = 20 },
+                new Metric { Timestamp = _now.AddSeconds(-30), Value = 30 },
+                new Metric { Timestamp = _now.AddSeconds(-20), Value = 40 },
+                new Metric { Timestamp = _now.AddSeconds(-10), Value = 50 },
+                new Metric { Timestamp = _now, Value = 60 }
+            ])
+        };
+
+        // Act
+        var result = Subject.Apply(aggregate, aggregate.RecentReadings.Last());
+
+        // Assert
+        await Assert.That(result.OneMinuteAverages.Count).IsEqualTo(1);
+        await Assert.That(result.OneMinuteAverages.First().Value).IsEqualTo(35); // Average of the values
+    }
+
+    public async Task OneMinuteAverageRule_Decimal_CalculatesCorrectOneMinuteAverage()
+    {
+        // Arrange
+        var aggregate = new MetricAggregate
+        {
+            RecentReadings = new ConcurrentQueue<Metric>([
+                new Metric { Timestamp = _now.AddSeconds(-50), Value = 10.1 },
+                new Metric { Timestamp = _now.AddSeconds(-40), Value = 20.2 },
+                new Metric { Timestamp = _now.AddSeconds(-30), Value = 30.3 },
+                new Metric { Timestamp = _now.AddSeconds(-20), Value = 40.4 },
+                new Metric { Timestamp = _now.AddSeconds(-10), Value = 50.5 },
+                new Metric { Timestamp = _now, Value = 60.6 }
+            ])
+        };
+
+        // Act
+        var result = Subject.Apply(aggregate, aggregate.RecentReadings.Last());
+
+        // Assert
+        await Assert.That(result.OneMinuteAverages.Count).IsEqualTo(1);
+        await Assert.That(Math.Abs(result.OneMinuteAverages.First().Value - 35.35)).IsLessThan(float.Epsilon); // Average of the values
+    }
+
+    #endregion
 
 }
