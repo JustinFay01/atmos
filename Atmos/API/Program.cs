69409c9f31af21458ec9bd7f843a08bcfb904c2e
Justin Fay <justin.fay@vervint.com>
2025-06-13 14:54:49 -0400

    React init and static host setup

--- Atmos/API/Program.cs
+++ Atmos/API/Program.cs
@@ -15,20 +15,23 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        
+
         builder.UseAtmosIssueTracker();
-        
+
         builder.Services.AddSignalR();
         builder.Services.AddSingleton<IRealtimeUpdateNotifier, RealTimeUpdateNotifier>();
         builder.Services.UseAtmosInfrastructure();
         builder.Services.UseAtmosApplicationServices();
         builder.Services.AddHostedService<SensorPollingWorker>();
+        builder.Services.AddControllers();
 
         var app = builder.Build();
-        
+        app.UseStaticFiles();
+
+        app.MapFallbackToFile("index.html");
 
         app.MapHub<DashboardHub>("/dashboard");
-
+        app.MapControllers();
         app.Run();
     }
 }
