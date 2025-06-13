c2eb5c6d7136c47bb3e8ad6fda22795c844a540a
Justin Fay <justin.fay@vervint.com>
2025-06-13 08:40:55 -0400

    Script updates

--- scripts/dotnet_format.sh
+++ scripts/dotnet_format.sh
@@ -7,17 +7,17 @@ echo "🚀 Running pre-commit dotnet format"
 # Get the list of staged MAUI-related files
 FILES=$(git diff --cached --name-only --diff-filter=ACM "*.cs" | sed 's| |\\ |g')
 
-if [ -z "$files" ]; then
+if [ -z "$FILES" ]; then
   echo "🤖 No staged C#, XAML, or Razor files to format. Committing Changes."
   exit 0
 fi
 
 # Run dotnet format
 echo "🔍 Running dotnet format..."
-echo "$FILES" | cat | xargs | sed -e 's/ /,/g' | xargs dotnet format src/StrideStudio --no-restore --include
+echo "$FILES" | cat | xargs | sed -e 's/ /,/g' | xargs dotnet format Atmos/ --no-restore --include
 
 # Re-add potentially modified files
-echo "$files" | while read -r line; do
+echo "$FILES" | while read -r line; do
     git add "$line"
 done
 
