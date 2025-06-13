fa26cd48505b65ba8abace8ac83800d6993231f9
Justin Fay <justin.fay@vervint.com>
2025-06-13 10:38:21 -0400

    Sentry integration

---
+++ Atmos/API/Extensions/AtmosIssueTracker.cs
@@ -0,0 +1,42 @@
+namespace API.Extensions;
+
+public static class AtmosIssueTracker
+{
+    /// <summary>
+    /// Sentry wrapper for the Atmos Issue Tracker.
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
+    public static void UseAtmosIssueTracker(this WebApplicationBuilder builder)
+    {
+        var sentryConfig = builder.Configuration.GetSection("Sentry");
+        var dsn = sentryConfig.GetValue<string>("Dsn");
+        var enabled = sentryConfig.GetValue<bool>("Enabled");
+        
+        if (!enabled)
+        {
+            Console.WriteLine("Sentry is disabled in the configuration.");
+            return;
+        }
+        
+        if (string.IsNullOrEmpty(dsn))
+        {
+            throw new ArgumentException("Sentry DSN is not configured in the application settings.");
+        }
+
+        SentrySdk.Init(options =>
+        {
+            options.Dsn = dsn;
+            options.Debug = true;
+            options.SendDefaultPii = true;
+        });
+        
+        builder.ConfigureSentryLogging();
+    }
+
+    private static void ConfigureSentryLogging(this WebApplicationBuilder builder)
+    {
+        builder.Logging.AddConfiguration(builder.Configuration);
+        builder.Logging.AddSentry();
+    }
+}
