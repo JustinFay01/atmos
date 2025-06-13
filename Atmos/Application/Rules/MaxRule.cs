4a4bc27c85dcd151155a7871bf3d85cc21c37891
Justin Fay <justin.fay@vervint.com>
2025-06-12 23:07:42 -0400

    Reset daily maximum and minimum values in MaxRule and MinRule when the day changes

--- Atmos/Application/Rules/MaxRule.cs
+++ Atmos/Application/Rules/MaxRule.cs
@@ -6,6 +6,14 @@ public class MaxRule : IMetricUpdateRule
 {
     public MetricAggregate Apply(MetricAggregate aggregate, Metric newMetric)
     {
+        if (aggregate.MaxValue.Timestamp.Day != newMetric.Timestamp.Day)
+        {
+            // Reset the daily maximum if the day has changed
+            return aggregate.CopyWith(
+                maxValue: newMetric
+            );
+        }
+
         var abs = Math.Abs(Math.Max(aggregate.MaxValue.Value, newMetric.Value) - aggregate.MaxValue.Value);
         var newAggregate = aggregate.CopyWith(
             maxValue: abs < float.Epsilon ?
--- Atmos/Application/Rules/MinRule.cs
+++ Atmos/Application/Rules/MinRule.cs
@@ -6,6 +6,14 @@ public class MinRule : IMetricUpdateRule
 {
     public MetricAggregate Apply(MetricAggregate aggregate, Metric newMetric)
     {
+        if (aggregate.MinValue.Timestamp.Day != newMetric.Timestamp.Day)
+        {
+            // Reset the daily minimum if the day has changed
+            return aggregate.CopyWith(
+                minValue: newMetric
+            );
+        }
+
         var abs = Math.Abs(Math.Min(aggregate.MinValue.Value, newMetric.Value) - aggregate.MinValue.Value);
         var newAggregate = aggregate.CopyWith(
             minValue: abs < float.Epsilon ?
