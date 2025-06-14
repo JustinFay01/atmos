78796a034502f333348b438a89067cdf2897d9a1
Justin Fay <justin.fay@vervint.com>
2025-06-13 23:38:51 -0400

    feat: add Sentry Vite plugin for error tracking and enable source maps in build

--- Atmos/API/atmos-client/vite.config.ts
+++ Atmos/API/atmos-client/vite.config.ts
@@ -1,8 +1,16 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react(), viteTsconfigPaths(), sentryVitePlugin({
+    org: "jnfcorp",
+    project: "atmos-client"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 });
